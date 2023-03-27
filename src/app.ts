836b6cd22fd3f7955f368cc4e86bb793f75e4013
Galla <gallant.thomas@outlook.com>
2023-03-26 21:12:10 -0400

    Adding warming function

--- src/app.ts
+++ src/app.ts
@@ -33,6 +33,10 @@ app.use(cors({
 
 const baseRoute: string = "/api/v1";
 
+app.get(baseRoute, function (req, res) {
+    res.send("Puzzle");
+});
+
 const globalRouter = require('./app/routes/routes');
 app.use(baseRoute, globalRouter);
 
