ce248ebda78be77cb21004f5ce1823961293d22d
Galla <gallant.thomas@outlook.com>
2023-04-09 13:10:25 -0400

    Adjusting formula again

--- src/app/services/userActiveGamesBFF.service.ts
+++ src/app/services/userActiveGamesBFF.service.ts
@@ -221,8 +221,8 @@ async function endGameService(puzzle, req) {
     // Score is rounded to the nearest integer
 
     let difficultyScore:number = (activeGameResponseBody[0].difficulty / 1000 * 35);
-    let hintsUsedScore:number = (activeGameResponseBody[0].numHintsUsed > 30) ? 0 : activeGameResponseBody[0].numHintsUsed;
-    let numWrongCellsPlayedScore:number = (activeGameResponseBody[0].numWrongCellsPlayed > 10) ? 0 : activeGameResponseBody[0].numWrongCellsPlayed;
+    let hintsUsedScore:number = (activeGameResponseBody[0].numHintsUsed > 30) ? 0 : (30 - activeGameResponseBody[0].numHintsUsed);
+    let numWrongCellsPlayedScore:number = (activeGameResponseBody[0].numWrongCellsPlayed > 10) ? 0 : (10 - activeGameResponseBody[0].numWrongCellsPlayed);
     let timeScore:number = (35 - (activeGameResponseBody[0].currentTime/60));
 
     let score:number = Math.round(difficultyScore + hintsUsedScore + numWrongCellsPlayedScore + timeScore);
